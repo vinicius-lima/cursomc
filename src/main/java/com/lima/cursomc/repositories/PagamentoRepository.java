9ea1f465f8d605bcc28b904877c35a8b190e5d62
Vinicius Lima <viniciusb80@gmail.com>
2018-11-28 16:22:10 -0200

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/lima/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.lima.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lima.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+
+}
