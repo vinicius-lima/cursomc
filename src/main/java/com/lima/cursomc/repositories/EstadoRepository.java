257f008455bb240694f7905ed3a6355eac2d81bb
Vinicius Lima <viniciusb80@gmail.com>
2018-11-28 11:13:32 -0200

    Estado e cidade

---
+++ src/main/java/com/lima/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.lima.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lima.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
