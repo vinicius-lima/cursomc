62d0b58db2dd76107dc41f634b9d376d17ff6730
Vinicius Lima <viniciusb80@gmail.com>
2018-11-29 16:13:30 -0200

    Personalizando o retorno da validacao

--- src/main/java/com/lima/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/lima/cursomc/resources/exception/StandardError.java
@@ -2,7 +2,7 @@ package com.lima.cursomc.resources.exception;
 
 import java.io.Serializable;
 
-public class StandardError implements Serializable{
+public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer status;
