a6ce4af39ca3d1c7036cc115bd84648d15890b36
Vinicius Lima <viniciusb80@gmail.com>
2018-12-07 08:45:51 -0200

    Email HTML

--- src/main/java/com/lima/cursomc/services/EmailService.java
+++ src/main/java/com/lima/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.lima.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.lima.cursomc.domain.Pedido;
@@ -7,4 +9,6 @@ import com.lima.cursomc.domain.Pedido;
 public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	void sendEmail(SimpleMailMessage msg);
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 }
