7556b0d0c651acb38cbe0d3dac50f4c08d6bb7f1
Vinicius Lima <viniciusb80@gmail.com>
2018-12-10 14:49:52 -0200

    Esqueci a senha

--- src/main/java/com/lima/cursomc/services/EmailService.java
+++ src/main/java/com/lima/cursomc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.lima.cursomc.domain.Cliente;
 import com.lima.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -11,4 +12,5 @@ public interface EmailService {
 	void sendEmail(SimpleMailMessage msg);
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	void sendHtmlEmail(MimeMessage msg);
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
