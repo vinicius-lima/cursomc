680f5abe24be8346ed0b2ecb57abe7e03539061e
Vinicius Lima <viniciusb80@gmail.com>
2018-12-10 16:39:41 -0200

    Enviando imagem via endpoint

--- src/main/java/com/lima/cursomc/services/ClienteService.java
+++ src/main/java/com/lima/cursomc/services/ClienteService.java
@@ -1,5 +1,7 @@
 package com.lima.cursomc.services;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +14,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.lima.cursomc.domain.Cidade;
 import com.lima.cursomc.domain.Cliente;
@@ -38,6 +41,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	//@Autowired
+	//private S3Service s3Service;
 
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -107,6 +113,15 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		//return s3Service.uploadFile(multipartFile);
+		try {
+			return new URI("http://not.implemented.yet/not-yet");
+		} catch (URISyntaxException e) {
+			throw new RuntimeException("Erro ao converter URL para URI");
+		}
 	}
 	
 }
